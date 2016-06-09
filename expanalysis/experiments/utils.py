196068aa5d5a7d6bcd0c69dcdacbafdb872b330a
ian <ianeisenberg90@gmail.com>
2016-06-08 22:38:58 -0700

    added anonymize function

--- expanalysis/experiments/utils.py
+++ expanalysis/experiments/utils.py
@@ -198,4 +198,6 @@ def result_filter(data, battery = None, exp_id = None, worker = None, template =
             data = select_finishtime(data, finishtime[0], finishtime[1])
     return data
 
-
+def anonymize_data(data):
+    workers = data.worker_id.unique()
+    data.replace(workers, ['s' + str(x).zfill(3) for x in range(len(workers))], inplace = True)
