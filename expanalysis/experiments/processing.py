69aef4e288c789116a1749485099fe3739d6d4ed
ian <ianeisenberg90@gmail.com>
2016-11-01 22:53:17 -0700

    added hddm analysis

--- expanalysis/experiments/processing.py
+++ expanalysis/experiments/processing.py
@@ -88,7 +88,7 @@ def get_drop_rows(exp_id):
                 'columbia_card_task_hot': {'trial_id': gen_cols + ['calculate reward', 'reward', 'test_intro']}, 
                 'dietary_decision': {'trial_id': gen_cols + ['start_taste', 'start_health']}, 
                 'digit_span': {'trial_id': gen_cols + ['start_reverse', 'stim', 'feedback']},
-                'directed_forgetting': {'trial_id': gen_cols + ['ITI_fixation', 'intro_test', 'stim', 'cue']},
+                'directed_forgetting': {'trial_id': gen_cols + ['ITI_fixation', 'intro_test', 'stim', 'cue', 'instruction_images']},
                 'dot_pattern_expectancy': {'trial_id': gen_cols + ['instruction_images', 'rest', 'cue', 'feedback']},
                 'go_nogo': {'trial_id': gen_cols + ['reset_trial']},
                 'hierarchical_rule': {'trial_id': gen_cols + ['feedback', 'test_intro']},
